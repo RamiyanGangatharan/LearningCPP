af1cc8e14e0b71c339361211c65aa2039294d3ea
Ramiyan Gangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2025-01-24 20:08:50 -0500

    Update basicArithmetic.h

--- calculator/basicArithmetic.h
+++ calculator/basicArithmetic.h
@@ -3,10 +3,10 @@
 #define BASIC_ARITHMETIC_H
 
 // Function Declarations
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-int divide(int a, int b);
+int addition (int a, int b);
+int subtract (int a, int b);
+int multiply (int a, int b);
+int divide   (int a, int b);
 
 void handleArithmetic();
 
